42aefc16337fab9bdf3aa6baf19a57c0bb51a32e
sammy0101 <59214670+sammy0101@users.noreply.github.com>
2025-12-17 23:15:10 +0800

    Update pdf.js

--- src/ui/pdf.js
+++ src/ui/pdf.js
@@ -18,6 +18,11 @@ export const pdfScript = `
             let yPos = height - 60;
             const marginX = 40;
 
+            // 定義顏色
+            const colorBlack = rgb(0, 0, 0);
+            const colorGreen = rgb(0, 0.5, 0);      // 收入綠
+            const colorOrange = rgb(0.85, 0.5, 0);  // 交通橙 (調深一點以便閱讀)
+
             const monthStr = document.getElementById('queryMonth').value;
             page.drawText(monthStr, { x: marginX, y: yPos, size: 20, font: helveticaBold });
             page.drawText(' OT/當更/交通 記錄表', { x: marginX + 90, y: yPos, size: 20, font: chineseFont });
@@ -25,7 +30,7 @@ export const pdfScript = `
 
             const col = { d: 40, item: 130, detail: 350, val: 480 };
             const fontSize = 11;
-            const drawTxt = (text, x, font, color=rgb(0,0,0)) => 
+            const drawTxt = (text, x, font, color=colorBlack) => 
                 page.drawText(text, { x, y: yPos, size: fontSize, font, color });
 
             drawTxt('日期', col.d, chineseFont, rgb(0.5,0.5,0.5));
@@ -41,19 +46,21 @@ export const pdfScript = `
                 if (r.type !== 'hourly' && amount === 0) continue;
 
                 let itemStr = '', detailStr = '', valStr = '';
-                let isMoney = false;
                 let detailFont = helvetica; 
+                let rowColor = colorBlack; // 預設黑色 (OT)
 
                 if (r.type === 'hourly') {
                     itemStr = r.location || 'OT';
                     const mins = getMinutesDiff(r.start, r.end);
                     detailStr = \`\${r.start.replace(':','')} - \${r.end.replace(':','')}\`;
                     valStr = formatHours(mins) + ' hr';
+                    rowColor = colorBlack;
                 } else if (r.type === 'transport') {
                     itemStr = '交通費';
                     detailStr = r.location ? \`(\${r.location})\` : '-';
                     detailFont = chineseFont; 
                     valStr = '$' + amount;
+                    rowColor = colorOrange; // === 交通費設為橙色 ===
                 } else if (r.type === 'oncall') {
                     itemStr = '當更 On-Call';
                     const startD = r.date.split('-')[2];
@@ -61,13 +68,13 @@ export const pdfScript = `
                     detailStr = \`\${startD}日 - \${endD}日\`;
                     detailFont = chineseFont;
                     valStr = '$' + amount;
-                    isMoney = true;
+                    rowColor = colorGreen; // === 收入設為綠色 ===
                 } else { 
                     itemStr = 'Call';
                     detailStr = r.location ? \`(\${r.location})\` : '-';
                     detailFont = chineseFont;
                     valStr = '$' + amount;
-                    isMoney = true;
+                    rowColor = colorGreen; // === 收入設為綠色 ===
                 }
 
                 drawTxt(r.date, col.d, helvetica);
@@ -76,7 +83,9 @@ export const pdfScript = `
                 drawTxt(safeItem, col.item, chineseFont);
                 
                 drawTxt(detailStr, col.detail, detailFont);
-                drawTxt(valStr, col.val, helveticaBold, isMoney ? rgb(0,0.5,0) : rgb(0,0,0));
+                
+                // 使用上面判斷好的顏色
+                drawTxt(valStr, col.val, helveticaBold, rowColor);
 
                 page.drawLine({ start: { x: marginX, y: yPos-8 }, end: { x: width-marginX, y: yPos-8 }, thickness: 0.5, color: rgb(0.9,0.9,0.9) });
                 yPos -= 25;
@@ -88,22 +97,25 @@ export const pdfScript = `
             page.drawLine({ start: { x: marginX, y: yPos }, end: { x: width-marginX, y: yPos }, thickness: 1 });
             yPos -= 25;
 
+            // 總時數
             drawTxt("總時數: ", 350, chineseFont);
             drawTxt(formatHours(grandTotalMinutes) + " hr", 410, helveticaBold);
             yPos -= 20;
 
+            // 總收入
             drawTxt("總收入: ", 350, chineseFont);
-            drawTxt("$" + grandTotalMoney, 410, helveticaBold, rgb(0,0.5,0));
+            drawTxt("$" + grandTotalMoney, 410, helveticaBold, colorGreen); // 綠色
             yPos -= 20;
 
+            // 總交通
             drawTxt("總交通: ", 350, chineseFont);
-            drawTxt("$" + grandTotalTransport, 410, helveticaBold); 
+            drawTxt("$" + grandTotalTransport, 410, helveticaBold, colorOrange); // 橙色
             yPos -= 20;
 
-            // === 新增：總計 ===
+            // 總計 (黑色)
             const totalAll = grandTotalMoney + grandTotalTransport;
             drawTxt("總計:", 350, chineseFont); 
-            drawTxt("$" + totalAll, 410, helveticaBold); 
+            drawTxt("$" + totalAll, 410, helveticaBold, colorBlack); 
             
             const pdfBytes = await pdfDoc.save();
             const blob = new Blob([pdfBytes], { type: 'application/pdf' });
